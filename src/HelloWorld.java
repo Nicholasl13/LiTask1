a92a75976eed0aa337538055ac269947a52adae5
1E10 Laptop <sample@bths.edu>
2022-09-21 10:34:26 -0400

    2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
 }
